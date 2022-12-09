e3d6659cb9dce8e0eee3c4501394a119563bdb27
yurchenko08 <yurchenko08@gmail.com>
2022-12-09 20:51:09 +0200

    finished task1

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,21 +1,78 @@
-import React, { Component } from 'react';
-
+/* eslint-disable no-fallthrough */
+import React, { useEffect, useRef } from 'react';
+import { useState } from 'react';
 import './input.css';
 
+function Input() {
+  const [operatorNumber, setOperatorNumber] = useState('');
+  const [num, setNum] = useState('');
+  const [isOperatorNum, setIsOperatorNum] = useState(false);
+  const [isNum, setIsNum] = useState(false);
+  const inputRef = useRef();
+  //subtask2
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  //subtask1
+  const handleInputOperator = (e) => {
+    setOperatorNumber(e.target.value.replace(/[^0-9]/g, ''));
+  };
+  useEffect(() => {
+    if (operatorNumber.length === 2) {
+      setIsOperatorNum(true);
+    } else setIsOperatorNum(false);
+  }, [operatorNumber]);
+  //subtask4
+  useEffect(() => {
+    if (num.length === 7) {
+      setIsNum(true);
+    } else setIsNum(false);
+  }, [num]);
 
-export default class Input extends Component{
-     render(){
-        return <div>
-            <span data-testid="operator-name"></span>
-            <span>+38 0</span>
-            <input 
-                type="text" 
-                data-testid="operator-input"
-                />
-            <span data-testid="check-icon"> - </span>
-            <input type="text" 
-                data-testid="phone-input"
-                 />
-        </div>;
+  const handleChangeNum = (e) => {
+    setNum(e.target.value.replace(/[^0-9]/g, ''));
+    //subtask4
+    if (num.length === 7) {
+      setIsNum(true);
+    } else {
+      setIsNum(false);
     }
+  };
+
+  //subtask3
+  const operators = {
+    Kyivstar: ['67', '68', '96', '97', '98'],
+    Vodafone: ['50', '66', '95', '99'],
+    Lifecell: ['63', '73', '93'],
+    '3mob': ['91'],
+    'People.net': ['92'],
+    intertelecom: ['89', '94'],
+  };
+  const searchOperator = (operatorNumber) => {
+    for (const operator in operators) {
+      if (operatorNumber && operators[operator].includes(operatorNumber))
+        return operator;
+      continue;
+    }
+  };
+
+  return (
+    <div>
+      <span data-testid='operator-name'>
+        {isOperatorNum && (searchOperator(operatorNumber) || 'Unknown')}
+      </span>
+      <span> +38 0</span>
+      <input
+        ref={inputRef}
+        onInput={handleInputOperator}
+        type='text'
+        data-testid='operator-input'
+      />
+      <span data-testid='check-icon'>
+        {isNum && isOperatorNum ? '✔️' : ' - '}
+      </span>
+      <input onInput={handleChangeNum} type='text' data-testid='phone-input' />
+    </div>
+  );
 }
+export default Input;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './components/app/app.js';
 
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
